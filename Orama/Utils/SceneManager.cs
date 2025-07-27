915097ee58bbe4c2f1fe8b0468ed7c69aea396a3
atskas <atskas0@gmail.com>
2025-07-27 16:36:48 +0300

    Add XML comments to `SceneManager.cs`

--- Orama/Utils/SceneManager.cs
+++ Orama/Utils/SceneManager.cs
@@ -7,16 +7,19 @@ namespace Orama.Utils;
 /// </summary>
 public static class SceneManager
 {
+	/// <summary> The currently active Scene. </summary>
     public static Scene Current { get; private set; } = new Scene();
-    public static Scene Scene => Current;
     
+    /// <summary> Initialize the Scene Manager. </summary>
     public static void Initialize() { }
 
+    /// <summary> Creates a new Scene. </summary>
     public static void NewScene()
     {
         Clear();
     }
 
+    /// <summary> Clears the current Scene. </summary>
     public static void Clear()
     {
         if (Current != null)
@@ -26,6 +29,7 @@ public static class SceneManager
         }
     }
 
+    /// <summary> Runs each frame. </summary>
     public static void Update()
     {
     }
