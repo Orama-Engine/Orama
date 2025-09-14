22b7a56c135f2120838325cbbf956e1340d880bd
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-09-14 20:29:08 +1000

    Add Implicit Components.

--- Orama/Entities/Entity.cs
+++ Orama/Entities/Entity.cs
@@ -2,6 +2,7 @@ using System.Numerics;
 using System.Collections.ObjectModel;
 using Orama.Components;
 using Orama.Echo;
+using System.Reflection;
 
 namespace Orama.Entities;
 
@@ -11,6 +12,7 @@ namespace Orama.Entities;
 /// </summary>
 public class Entity : ISerializationCallbackReceiver
 {
+	/// <summary> The name of the Entity. </summary>
 	public string Name { get; set; } = "Entity";
 
 	public Transform Transform { get; set; } = new();
@@ -20,8 +22,16 @@ public class Entity : ISerializationCallbackReceiver
 	/// <summary> Read-only collection of the Entity's Components </summary>
 	[SerializeIgnore] public ReadOnlyCollection<Component> Components => components.AsReadOnly();
 	
-	public Entity() { }
-	public Entity(string name) => Name = name;
+	public Entity()
+	{
+		AddImplicitComponents();
+	}
+
+	public Entity(string name)
+	{
+		Name = name;
+		AddImplicitComponents();
+	}
 
 
 	/// <summary> Runs when the Entity is created. </summary>
@@ -72,5 +82,38 @@ public class Entity : ISerializationCallbackReceiver
 		// Reconstruct all component instances
 		foreach (var component in components)
 			component.Entity = this;
+	}
+
+	private void AddImplicitComponents()
+	{
+		// Reflect and add all implicit components
+		foreach (var field in GetType().GetFields())
+		{
+			var attribute = field.GetCustomAttribute<ImplicitComponentAttribute>();
+			if (attribute != null)
+			{
+				// Create the component
+				var component = (Component)Activator.CreateInstance(field.FieldType)!;
+				component.Entity = this;
+				components.Add(component);
+
+				// Set the field
+				field.SetValue(this, component);
+			}
+		}
+
+		foreach (var property in GetType().GetProperties())
+		{
+			var attribute = property.GetCustomAttribute<ImplicitComponentAttribute>();
+			if (attribute != null)
+			{
+				// Create the component
+				var component = (Component)Activator.CreateInstance(property.PropertyType)!;
+				component.Entity = this;
+				components.Add(component);
+
+				property.SetValue(this, component);
+			}
+		}
 	}
 }
