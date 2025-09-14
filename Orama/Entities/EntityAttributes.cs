11c2ad620def3890846d89d765877d75bb059085
atskas <atskas0@gmail.com>
2025-09-14 14:08:50 +0300

    Merge remote-tracking branch 'origin/modular'
    # Conflicts:
    #	Launchers/Orama.Desktop/Program.cs
    #	Orama/Modules/Audio/AudioModule.cs

---
+++ Orama/Entities/EntityAttributes.cs
@@ -0,0 +1,15 @@
+﻿using Orama.Components;
+
+namespace Orama.Entities;
+
+// TODO: Maybe move this to component attributes?
+
+/// <summary>
+/// Marks a <see cref="Component"/> variable within an <see cref="Entity"/> as implicit.
+/// </summary>
+/// <remarks>
+/// Implicit Components will be added to the Entity automatically when the Entity is created
+/// and are hidden in the inspector.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class ImplicitComponentAttribute : Attribute { }
