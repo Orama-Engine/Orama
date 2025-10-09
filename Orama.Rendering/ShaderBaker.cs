c3e35da38b6e64b595f04baf7a0356a3ab1c28b3
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-10-09 22:34:59 +1100

    Clarify `ShaderBaker` purpose.

--- Orama.Rendering/ShaderBaker.cs
+++ Orama.Rendering/ShaderBaker.cs
@@ -4,13 +4,13 @@ using Orama.Rendering.Resources;
 namespace Orama.Rendering;
 
 /// <summary>
-/// Responsible for converting shaders from source to Spir-V.
+/// Responsible for converting shaders from source to compiled shaders.
 /// </summary>
 public static class ShaderBaker
 {
-    /// <summary> Converts GLSL source to Spir-V. </summary>
-    public static GraphicsShader GLSLToSpirV(string source) => new();
+    /// <summary> Compiles GLSL source to a <see cref="GraphicsShader"/>. </summary>
+    public static GraphicsShader GLSLToShader(string source) => new();
 
-    /// <summary> Converts HLSL source to Spir-V. </summary>
-    public static GraphicsShader HLSLToSpirV(string source) => new();
+    /// <summary> Compiles HLSL source to a <see cref="GraphicsShader"/>. </summary>
+    public static GraphicsShader HLSLToShader(string source) => new();
 }
