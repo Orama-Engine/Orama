7eb9d81296b43e8e2444417ef13e6212e8892e2f
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-10-10 22:36:42 +1100

    Start work on modules.

---
+++ Orama.Core/Modules/BaseModule.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Orama.Core.Modules;
+
+/// <summary>
+/// Base class for all modules.
+/// </summary>
+public class BaseModule
+{
+    /// <summary> Runs once at the start of the module. </summary>
+    public virtual void Initialize() { }
+
+    /// <summary> Runs every frame. </summary>
+    public virtual void Update() { }
+
+    /// <summary> Runs once at the end of the module. </summary>
+    public virtual void Dispose() { }
+}
---
+++ Orama.Core/Modules/ModuleManager.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Orama.Core.Modules;
+
+/// <summary>
+/// Management of the module system.
+/// </summary>
+public static class ModuleManager
+{
+    /// <summary> All currently registered modules. </summary>
+    public static IReadOnlyDictionary<Type, BaseModule> RegisteredModules => registeredModules;
+
+    private static readonly Dictionary<Type, BaseModule> registeredModules = new Dictionary<Type, BaseModule>();
+
+    /// <summary> Gets a module of type T if it is registered. </summary>
+    /// <typeparam name="T"> The type of the module. </typeparam>
+    /// <remarks> If you need a non nullable return value, use <see cref="GetOrRegisterModule{T}"/> instead. </remarks>
+    public static T? GetModule<T>() where T : BaseModule => (T?)(registeredModules.TryGetValue(typeof(T), out var module) ? module : null);
+
+    /// <summary> Gets a module of type T or registers it if it is not registered. </summary>
+    /// <typeparam name="T"> The type of the module. </typeparam>
+    public static T GetOrRegisterModule<T>() where T : BaseModule, new()
+    {
+        if (GetModule<T>() is T module)
+            return module;
+
+        return RegisterModule<T>();
+    }
+
+    /// <summary> Registers a module of type T. </summary>
+    /// <typeparam name="T"> The type of the module. </typeparam>
+    public static T RegisterModule<T>() where T : BaseModule, new()
+    {
+        T module = new T();
+        registeredModules.Add(module.GetType(), module);
+
+        Application.OnUpdate += module.Update;
+        Application.OnStart += module.Initialize;
+        Application.OnExit += module.Dispose;
+
+        return module;
+    }
+
+    /// <summary> Unregisters a module of type T. </summary>
+    /// <typeparam name="T"> The type of the module. </typeparam>
+    public static void UnregisterModule<T>() where T : BaseModule => registeredModules.Remove(typeof(T));
+}
