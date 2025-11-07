04fb083ab2807340f650ab182cd792fac35f1269
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-11-07 17:40:27 +1100

    Start parsing `ShaderLangFormat`.

---
+++ Orama.ShaderLang/Parser/Lexer.cs
@@ -0,0 +1,114 @@
+﻿
+using System.Text;
+
+namespace Orama.ShaderLang.Parser;
+
+internal class Lexer
+{
+    private readonly string source;
+    private int pos = 0;
+
+    /// <summary> Initializes a new instance of the <see cref="Lexer"/> class. </summary>
+    public Lexer(string source)
+    {
+        this.source = source;
+    }
+
+    /// <summary> Converts the source code into a list of tokens. </summary>
+    public List<Token> Tokenize()
+    {
+        List<Token> tokens = new List<Token>();
+
+        while (!IsAtEnd())
+        {
+            char c = Peek();
+
+            if (char.IsWhiteSpace(c))
+            {
+                Advance();
+                continue;
+            }
+
+            if (c == '{')
+            {
+                tokens.Add(new Token(TokenType.LeftBrace, "{"));
+                Advance();
+                continue;
+            }
+
+            if (c == '}')
+            {
+                tokens.Add(new Token(TokenType.RightBrace, "}"));
+                Advance();
+                continue;
+            }
+
+            if (c == '=')
+            {
+                tokens.Add(new Token(TokenType.Equals, "="));
+                Advance();
+                continue;
+            }
+
+            if (c == '#')
+            {
+                tokens.Add(new Token(TokenType.Hash, "#"));
+                Advance();
+                continue;
+            }
+
+            if (c == '"')
+            {
+                tokens.Add(ReadString());
+                continue;
+            } 
+            else if (char.IsDigit(c))
+            {
+                tokens.Add(ReadNumber());
+                continue;
+            } else
+            {
+                tokens.Add(ReadIdentifier());
+                continue;
+            }
+        }
+
+        tokens.Add(new Token(TokenType.EndOfFile, ""));
+        return tokens;
+    }
+
+    private Token ReadString()
+    {
+        Advance(); // Skip initial quote
+
+        StringBuilder sb = new StringBuilder();
+        while (Peek() != '"' && !IsAtEnd())
+            sb.Append(Advance());
+
+        Advance(); // Skip final quote
+
+        return new Token(TokenType.String, sb.ToString());
+    }
+
+    private Token ReadNumber()
+    {
+        StringBuilder sb = new StringBuilder();
+        while (char.IsDigit(Peek()))
+            sb.Append(Advance());
+
+        return new Token(TokenType.Number, sb.ToString());
+    }
+
+    private Token ReadIdentifier()
+    {
+        StringBuilder sb = new StringBuilder();
+        while (char.IsLetter(Peek()))
+            sb.Append(Advance());
+
+        return new Token(TokenType.Identifier, sb.ToString());
+    }
+
+    private char Peek() => source[pos];
+    private char Advance() => source[pos++];
+    private bool IsAtEnd() => pos >= source.Length;
+}
--- Orama.ShaderLang/ShaderLangFormat.cs
+++ Orama.ShaderLang/ShaderLangFormat.cs
@@ -1,4 +1,6 @@
-﻿namespace Orama.ShaderLang;
+﻿using Orama.ShaderLang.Parser;
+
+namespace Orama.ShaderLang;
 
 /// <summary>
 /// Data representation of an Orama shader format.
@@ -8,7 +10,30 @@ public class ShaderLangFormat
     /// <summary> Parse a shader from a string. </summary>
     public static ShaderLangFormat FromSource(string source)
     {
-        return new ShaderLangFormat();
+        var shader = new ShaderLangFormat();
+
+        var lexer = new Lexer(source);
+        var tokens = lexer.Tokenize();
+
+        for (int i = 0; i < tokens.Count; i++)
+        {
+            var token = tokens[i];
+
+            if (token.Type == TokenType.Identifier && token.Value.Equals("pass", StringComparison.OrdinalIgnoreCase))
+            {
+                int valueIndex = i + 1;
+                if (valueIndex < tokens.Count && tokens[valueIndex].Type == TokenType.Equals)
+                    valueIndex++;
+
+                if (valueIndex < tokens.Count && (tokens[valueIndex].Type == TokenType.String || tokens[valueIndex].Type == TokenType.Identifier))
+                {
+                    shader.Pass = tokens[valueIndex].Value;
+                    i = valueIndex;
+                }
+            }
+        }
+
+        return shader;
     }
 
     /// <summary> The pass this shader belongs to. </summary>
