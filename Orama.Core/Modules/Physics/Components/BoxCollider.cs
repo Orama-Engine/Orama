f2ec57dd4599484e95bb947b29dfb67d12058eb1
Pan <76873505+atskas@users.noreply.github.com>
2025-11-07 17:25:18 +0200

    Fix `BoxCollider` scaling.

--- Orama.Core/Modules/Physics/Components/BoxCollider.cs
+++ Orama.Core/Modules/Physics/Components/BoxCollider.cs
@@ -7,10 +7,19 @@ using System.Text;
 
 namespace Orama.Core.Modules.Physics.Components;
 
+/// <summary>
+/// Represents a box-shaped collider component that defines the physical boundaries of an entity for collision
+/// detection.
+/// </summary>
 public class BoxCollider : Component
 {
+    /// <summary> Width of the box collider. </summary>
     public float Width { get; set; }
+
+    /// <summary> Height of the box collider. </summary>
     public float Height { get; set; }
+
+    /// <summary> Depth of the box collider. </summary>
     public float Depth { get; set; }
 
     public BoxCollider(float width, float height, float depth)
@@ -25,7 +34,7 @@ public class BoxCollider : Component
         if (Entity.GetComponent<RigidBody>() == null)
             return;
 
-        JVector size = new JVector(Width, Height, Depth);
+        JVector size = new JVector(Width * 2, Height * 2, Depth * 2);
         var boxShape = new BoxShape(size);
         Entity.GetComponent<RigidBody>()!.AddShape(boxShape);
     }
