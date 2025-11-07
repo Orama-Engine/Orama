4cfd9ad0bfb41f5c2dbde784f5952f7587d2ef50
Pan <76873505+atskas@users.noreply.github.com>
2025-11-07 20:20:01 +0200

    Create `SphereCollider`.

---
+++ Orama.Core/Modules/Physics/Components/SphereCollider.cs
@@ -0,0 +1,32 @@
+﻿using Jitter2.Collision.Shapes;
+using Jitter2.LinearMath;
+using Orama.Core.Common.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orama.Core.Modules.Physics.Components;
+
+/// <summary>
+/// Represents a sphere-shaped collider component that defines the physical boundaries of an entity for collision
+/// detection.
+/// </summary>
+public class SphereCollider : Component
+{
+    /// <summary> Radius of the sphere collider. </summary>
+    public float Radius { get; set; }
+
+    public SphereCollider(float radius)
+    {
+        Radius = radius;
+    }
+
+    public override void Start()
+    {
+        if (Entity.GetComponent<RigidBody>() == null)
+            return;
+
+        var sphereShape = new SphereShape(Radius);
+        Entity.GetComponent<RigidBody>()!.AddShape(sphereShape);
+    }
+}
