a69613287f465b556e09949382c10ec99bfa55bd
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-11-08 22:20:09 +1100

    Add `FontLoader`.

---
+++ Orama.Core/Common/Resources/DefaultProvider/FontLoader.cs
@@ -0,0 +1,33 @@
+﻿using Orama.Core.Modules.GUI.Resources;
+using SixLabors.Fonts;
+using Font = Orama.Core.Modules.GUI.Resources.Font;
+
+
+namespace Orama.Core.Common.Resources.DefaultProvider;
+
+[ResourceLoader]
+internal class FontLoader : ResourceLoader<Font>
+{
+    /// <inheritdoc/>
+    public override Font? LoadResource(byte[] data)
+    {
+        try
+        {
+            var collection = new FontCollection();
+
+            using var ms = new MemoryStream(data);
+            FontFamily family = collection.Add(ms);
+
+            // Default to 20pt
+            const int defaultSize = 19;
+
+            var sixFont = family.CreateFont(defaultSize);
+
+            return new Font(sixFont, defaultSize);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
