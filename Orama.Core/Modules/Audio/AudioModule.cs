47cfec25c3a752d95a88f2cc3f0a6d5f1bc1cf45
Pan <76873505+atskas@users.noreply.github.com>
2026-03-16 10:08:24 +0200

    Implement IAudioContext in AudioModule

--- Orama.Core/Modules/Audio/AudioModule.cs
+++ Orama.Core/Modules/Audio/AudioModule.cs
@@ -1,12 +1,25 @@
 ﻿
+using Orama.Core.Modules.Audio.Engines.OpenAL;
+
 namespace Orama.Core.Modules.Audio;
 
-public class AudioModule : BaseModule
+/// <summary>
+/// Module responsible for handling audio.
+/// </summary>
+public class AudioModule : BaseModule, IAudioContext
 {
-    /// <summary> The audio context. </summary>
-    public IAudioContext? Context { get; set; }
+    private readonly IAudioContext context;
+
+    /// <inheritdoc/>
+    public IAudioSource CreateSource() => context.CreateSource();
+
+    /// <inheritdoc/>
+    public void DestroySource(IAudioSource source) => context.DestroySource(source);
 
     /// <summary> Creates a new audio module with the given audio context. </summary>
     /// <param name="context"> The audio context to use. </param>
-    public AudioModule(IAudioContext context) => Context = context;
+    public AudioModule(IAudioContext context) => this.context = context;
+
+    /// <summary> Creates a new audio module with the default audio context. </summary>
+    public AudioModule() => this.context = new OpenALContext();
 }
