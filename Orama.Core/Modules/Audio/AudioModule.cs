74ef80cb1b5d6b2ac99af5bef458a48337c098fe
Pan <76873505+atskas@users.noreply.github.com>
2026-03-16 09:53:04 +0200

    Begin working on `AudioModule` and its structure.

---
+++ Orama.Core/Modules/Audio/AudioModule.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Orama.Core.Modules.Audio;
+
+public class AudioModule : BaseModule
+{
+    /// <summary> The audio context. </summary>
+    public IAudioContext? Context { get; set; }
+
+    /// <summary> Creates a new audio module with the given audio context. </summary>
+    /// <param name="context"> The audio context to use. </param>
+    public AudioModule(IAudioContext context) => Context = context;
+}
---
+++ Orama.Core/Modules/Audio/IAudioSource.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Orama.Core.Modules.Audio;
+
+/// <summary>
+/// Represents an audio source within the audio context.
+/// </summary>
+public interface IAudioSource
+{
+    /// <summary> Volume of the audio source. </summary>
+    float Volume { get; set; }
+
+    /// <summary> Pitch of the audio source. </summary>
+    float Pitch { get; set; }
+
+    /// <summary> Whether the audio source loops. </summary>
+    bool Loop { get; set; }
+
+    /// <summary> Whether the audio source is currently playing. </summary>
+    bool IsPlaying { get; }
+
+
+    /// <summary> Plays the audio source. </summary>
+    void Play();
+
+    /// <summary> Stops the audio source. </summary>
+    void Stop();
+}
