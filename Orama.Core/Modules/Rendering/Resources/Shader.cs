f373c2da65417c20250dd80042e00c061d26d4b3
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-05-30 13:55:47 +1000

    Add `Source` property to `Shader`.

--- Orama.Core/Modules/Rendering/Resources/Shader.cs
+++ Orama.Core/Modules/Rendering/Resources/Shader.cs
@@ -30,13 +30,30 @@ public sealed class ShaderParameter
 public class Shader
 {
     /// <summary> The name of the shader's pass. </summary>
-    public string Pass { get; }
+    public string Pass { get; private set; } = "None";
+
+    /// <summary> The shader's raw ShaderLang source. </summary>
+    public string Source
+    {
+        get => field;
+        set
+        {
+            ShaderLangFormat format = ShaderLangFormat.FromSource(value);
+            (string, string) hlsl = HLSLTarget.Compile(format);
+            Vertex = hlsl.Item1;
+            Fragment = hlsl.Item2;
+
+            Pass = format.Pass ?? "None";
+            
+            field = value;
+        }
+    }
 
     /// <summary> The shader's raw HLSL vertex shader source. </summary>
-    internal string Vertex {  get; }
+    internal string Vertex { get; private set; } = string.Empty;
 
     /// <summary> The shader's raw HLSL fragment shader source. </summary>
-    internal string Fragment { get; }
+    internal string Fragment { get; private set; } = string.Empty;
 
     /// <summary> The shader's parameter definitions. </summary>
     public IReadOnlyList<ShaderParameter> Parameters => parameters;
@@ -44,14 +61,5 @@ public class Shader
     private readonly List<ShaderParameter> parameters = new List<ShaderParameter>();
 
     /// <summary> Initializes a new <see cref="Shader"/> from the specified ShaderLang source. </summary>
-    public Shader(string shaderLangSource)
-    {
-        ShaderLangFormat format = ShaderLangFormat.FromSource(shaderLangSource);
-        (string, string) hlsl = HLSLTarget.Compile(format);
-
-        Vertex = hlsl.Item1;
-        Fragment = hlsl.Item2;
-
-        Pass = format.Pass ?? "None";
-    }
+    public Shader(string shaderLangSource) => Source = shaderLangSource;
 }
