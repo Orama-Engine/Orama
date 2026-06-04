1039330e6108bf3a10634f5ade2bf11ed6b07078
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-06-05 01:20:24 +1000

    Refactor `RigidBody`.

--- Orama.Core/Modules/Physics/Components/RigidBody.cs
+++ Orama.Core/Modules/Physics/Components/RigidBody.cs
@@ -15,72 +15,73 @@ public class RigidBody : Component
     public bool IsStatic
     {
         get => body?.IsStatic ?? false;
-        set { if (body != null) body.IsStatic = value; }
+        set => body?.IsStatic = value;
     }
 
     /// <inheritdoc/>
     public bool AffectedByGravity
     {
         get => body?.AffectedByGravity ?? false;
-        set { if (body != null) body.AffectedByGravity = value; }
+        set => body?.AffectedByGravity = value;
     }
 
     /// <inheritdoc/>
     public float Mass
     {
         get => body?.Mass ?? 0f;
-        set { if (body != null) body.Mass = value; }
+        set => body?.Mass = value;
     }
 
     /// <inheritdoc/>
     public float Friction
     {
         get => body?.Friction ?? 0f;
-        set { if (body != null) body.Friction = value; }
+        set => body?.Friction = value;
     }
 
     /// <inheritdoc/>
     public float Restitution
     {
         get => body?.Restitution ?? 0f;
-        set { if (body != null) body.Restitution = value; }
+        set => body?.Restitution = value;
     }
 
     /// <inheritdoc/>
     public Vector3 Position
     {
         get => body?.Position ?? Vector3.Zero;
-        set { if (body != null) body.Position = value; }
+        set => body?.Position = value;
     }
 
     /// <inheritdoc/>
     public Quaternion Orientation
     {
         get => body?.Orientation ?? new Quaternion(0, 0, 0, 1);
-        set { if (body != null) body.Orientation = value; }
+        set => body?.Orientation = value;
     }
 
     /// <inheritdoc/>
     public Vector3 Velocity
     {
         get => body?.Velocity ?? Vector3.Zero;
-        set { if (body != null) body.Velocity = value; }
+        set => body?.Velocity = value;
     }
 
     /// <inheritdoc/>
     public Vector3 AngularVelocity
     {
         get => body?.AngularVelocity ?? Vector3.Zero;
-        set { if (body != null) body.AngularVelocity = value; }
+        set => body?.AngularVelocity = value;
     }
 
     /// <inheritdoc/>
     public Vector2 Damping
     {
         get => body?.Damping ?? Vector2.Zero;
-        set { if (body != null) body.Damping = value; }
+        set => body?.Damping = value;
     }
 
+    /// <inheritdoc/>
     public override void Start()
     {
         var physics = ModuleManager.GetModule<PhysicsModule>();
@@ -90,39 +91,51 @@ public class RigidBody : Component
             body.Position = Entity.Transform.Position;
     }
 
+    /// <inheritdoc/>
     public override void Update()
     {
-        if (body != null)
-        {
-            Entity.Transform.Position = body.Position;
-            Entity.Transform.Rotation = body.Orientation;
-        }
+        if (body == null)
+            return;
+
+        Entity.Transform.Position = body.Position;
+        Entity.Transform.Rotation = body.Orientation;
     }
 
+    /// <inheritdoc/>
     public override void Destroy()
     {
         base.Destroy();
         var physics = ModuleManager.GetModule<PhysicsModule>();
-        if (body != null) physics?.World?.DestroyBody(body);
+
+        if (body != null)
+            physics?.World?.DestroyBody(body);
     }
 
     /// <inheritdoc/>
     public int AddBoxCollider(float width, float height, float depth)
     {
-        if (body == null) return 0;
+        if (body == null)
+            return 0;
 
         var id = body.AddBoxCollider(width, height, depth);
-        if (IsStatic) Orientation = Entity.Transform.Rotation;
+
+        if (IsStatic)
+            Orientation = Entity.Transform.Rotation;
+
         return id;
     }
 
     /// <inheritdoc/>
     public int AddSphereCollider(float radius)
     {
-        if (body == null) return 0;
+        if (body == null)
+            return 0;
 
         var id = body.AddSphereCollider(radius);
-        if (IsStatic) Orientation = Entity.Transform.Rotation;
+
+        if (IsStatic)
+            Orientation = Entity.Transform.Rotation;
+
         return id;
     }
 
@@ -142,7 +155,7 @@ public class RigidBody : Component
     /// <inheritdoc/>
     public event Action<IPhysicsBody>? OnCollisionExit
     {
-        add { if (body != null) body.OnCollisionExit += value; }
+        add{ if (body != null) body.OnCollisionExit += value; }
         remove { if (body != null) body.OnCollisionExit -= value; }
     }
 }
