1fb9884ab68a615b71e9d76595f47a4f8da39f4b
Tweety <allie.g@tweety-lab.dev>
2026-06-29 22:42:09 +1000

    Merge `CommandBufferExtensions` into `CommandBuffer`.

--- Modules/Orama.Rendering/Device/CommandBuffer.cs
+++ Modules/Orama.Rendering/Device/CommandBuffer.cs
@@ -1,7 +1,9 @@
+using Orama.Math;
 using Orama.Rendering.Resources;
 using Orama.Rendering.Resources.Caches;
 using Orama.Rendering.Veldrid;
 using System.Collections.Immutable;
+using System.Runtime.InteropServices;
 using Veldrid;
 
 namespace Orama.Rendering.Device;
@@ -16,12 +18,67 @@ public class CommandBuffer : IDisposable
     /// <summary> Initializes a new instance of the <see cref="CommandBuffer"/> class. </summary>
     public CommandBuffer(VeldridDevice device) => CommandList = device.GraphicsDevice.ResourceFactory.CreateCommandList();
 
+    private static Dictionary<uint, GPUBuffer> gpuBufferQueue = new Dictionary<uint, GPUBuffer>();
+
     /// <inheritdoc/>
     public void Dispose() => CommandList.Dispose();
 
     public void Begin() => CommandList.Begin();
 
     public void End() => CommandList.End();
+
+    public void ClearColor(Color color) => CommandList.ClearColorTarget(0, new global::Veldrid.RgbaFloat(color.R, color.G, color.B, color.A));
+
+    public void QueueGPUBuffer(GPUBuffer gpuBuffer, uint slot) => gpuBufferQueue[slot] = gpuBuffer;
+
+    public void DrawRenderable(IClientRenderable renderable, Matrix4x4 model)
+    {
+        var gd = Renderer.Veldrid.GraphicsDevice;
+
+        int vertCount = renderable.Vertices.Length;
+        float[] vertexData = new float[vertCount * 8];
+        for (int i = 0; i < vertCount; i++)
+        {
+            vertexData[i * 8 + 0] = renderable.Vertices[i].X;
+            vertexData[i * 8 + 1] = renderable.Vertices[i].Y;
+            vertexData[i * 8 + 2] = renderable.Vertices[i].Z;
+            vertexData[i * 8 + 3] = i < renderable.Normals.Length ? renderable.Normals[i].X : 0f;
+            vertexData[i * 8 + 4] = i < renderable.Normals.Length ? renderable.Normals[i].Y : 0f;
+            vertexData[i * 8 + 5] = i < renderable.Normals.Length ? renderable.Normals[i].Z : 1f;
+            vertexData[i * 8 + 6] = i < renderable.UVs.Length ? renderable.UVs[i].X : 0f;
+            vertexData[i * 8 + 7] = i < renderable.UVs.Length ? renderable.UVs[i].Y : 0f;
+        }
+
+        // index 0: MaterialParams
+        // index 1: ObjectParams
+        ResourceLayoutDescription layoutDesc = new(new ResourceLayoutElementDescription("MaterialParams", ResourceKind.UniformBuffer, ShaderStages.Vertex | ShaderStages.Fragment));
+
+        PipelineKey pipelineDesc = new PipelineKey(
+            PassName: renderable.Material.Pass,
+            Shader: new ShaderKey(renderable.Material.Shader.VertexBytecode, renderable.Material.Shader.FragmentBytecode),
+            Outputs: gd.SwapchainFramebuffer.OutputDescription,
+            ResourceLayout: layoutDesc
+        );
+
+        var vertexBytes = MemoryMarshal.AsBytes(vertexData.AsSpan()).ToArray();
+        var indexBytes = MemoryMarshal.AsBytes(renderable.Indices.AsSpan()).ToArray();
+
+        FrameCountedResource<RenderItem> item = RenderItemCache.Instance.GetOrCreate(new RenderItemKey(
+            VertexBuffer: new RIBufferKey(ImmutableCollectionsMarshal.AsImmutableArray(vertexBytes), BufferUsage.VertexBuffer),
+            IndexBuffer: new RIBufferKey(ImmutableCollectionsMarshal.AsImmutableArray(indexBytes), BufferUsage.IndexBuffer),
+            IndexCount: (uint)renderable.Indices.Length,
+            Pipeline: pipelineDesc
+        ));
+
+        SetPipeline(pipelineDesc);
+
+        foreach (var (slot, gpuBuffer) in gpuBufferQueue)
+            UploadUniformBuffer(Pipeline, (uint)gpuBuffer.Data.Count, slot, gpuBuffer.Data.ToArray());
+
+        gpuBufferQueue.Clear();
+
+        DrawItem(item.Resource);
+    }
 
     public void UploadUniformBuffer(PipelineKey target, uint size, uint index, ReadOnlySpan<byte> data)
     {
