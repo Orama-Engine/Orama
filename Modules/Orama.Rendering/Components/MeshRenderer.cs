0edcc61efad2280d5644cb0ba91f54753ca1a915
Tweety <allie.g@tweety-lab.dev>
2026-07-24 18:06:43 +1000

    Add warning when `MeshRenderer` has no `Mesh`.

--- Modules/Orama.Rendering/Components/MeshRenderer.cs
+++ Modules/Orama.Rendering/Components/MeshRenderer.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. (https://github.com/Orama-Engine/Orama/blob/main/LICENSE)
 
 using Orama.Common;
+using Orama.Common.Utility;
 using Orama.Rendering.Resources;
 using Orama.Scenes.Components;
 
@@ -12,9 +13,19 @@ public class MeshRenderer : Component
 	/// <summary> The <see cref="Resources.Mesh"/> to render. </summary>
 	public Mesh? Mesh { get; set; }
 
+	public override void Start()
+	{
+		base.Start();
+
+		if (Mesh == null)
+			OramaConsole.Warning($"MeshRenderer '{Entity.Name}' has no mesh.");
+	}
+
 	/// <inheritdoc/>
 	public override void Update()
 	{
+		base.Update();
+
 		if (Mesh == null)
 			return;
 
